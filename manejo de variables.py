053ca590f83c3e1912b26bf3b0f4e8650eb1aa43
Alejo <alejoivanrojas@gmail.com>
2022-09-23 16:17:52 -0300

    ejercicios de varibales

---
+++ manejo de variables.py
@@ -0,0 +1,75 @@
+
+
+
+suma = 20 + 30
+print("El resultado de la suma es:", suma)
+resta = 50-10
+print("El resultado de la resta es:", resta)
+
+division_flotante = 25/3
+print("Resultado de la division flotante es:", division_flotante)
+
+division_entera = 25 // 5
+print("Resultado de la division entera es:", division_entera)
+
+multiplicacion = 25 * 2
+print("Resultado de la multiplicacion es:", multiplicacion)
+
+modulo = 25 % 3
+print("Resultado del modulo:", modulo)
+
+potencia = 25 ** 2
+print("El resultado de la potencia es:", potencia)
+
+a = True
+b = True
+c = False
+x = a and b
+print("Resultado AND:", x)
+y = c or c
+print("Resultado OR;", y)
+z = not a
+print("Resultado NOT:", z)
+
+concatenacion = "hola" + " mundo ++"
+print("Resultado concatenacion:", concatenacion)
+
+multiplicacion = "hola" * 3
+print("Resultado multiplicacion:", multiplicacion)
+
+mezcla = "hola" * 3 + " mundo "
+print("Resultado mezcla:", mezcla)
+
+real = 20.5
+print(type(real))
+
+complejo = 2.1 + 6j
+print(type(complejo))
+
+cadena = "Hola Mundo"
+print(type(cadena))
+
+booleano = True
+print(type(booleano))
+
+conjunto = {"naranja", 1, "c", 2.5, "ciruela"}
+print(type(conjunto))
+
+lista = [0.5, "manzana", 1500, "azul"]
+print(type(lista))
+
+tupla = 1, 25, 1500, "Hola Mundo"
+tupla_anidada = tupla,True, "Hola", 5
+print(type(tupla))
+print(type(tupla_anidada))
+
+diccionario = {
+    1:"Susana Gimenez",
+    5:"Marcelo Tinelli",
+    7:"Mirtha Legrand"
+}
+print(type(diccionario))
+
+x = None
+print(type(x))
+
---
+++ prueba.py
@@ -0,0 +1,12 @@
+import py_compile
+
+
+Nombre = input("Ingrese su nombre:")
+Edad = int(input("Ingrese su edad:"))
+print (f"El usuario { Nombre } tiene { Edad } anos")
+
+
+
+
+
+
